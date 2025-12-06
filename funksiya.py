c36268d179a5d9b7f8a848c2e6a3af9f77057bb3
Bread <sherzodahmedov1007@gmail.com>
2025-12-06 17:42:16 +0500

    Add files via upload
    not done yet

---
+++ funksiya.py
@@ -0,0 +1,32 @@
+# 1 - modul: 1 - misol -------------------------------------------------------------
+
+# def kvadrat(son):
+#     return son ** 2
+
+# son = int(input("Sonni kiriting: "))
+# print(f"{son} ning kvadrati: {kvadrat(son)}")
+
+# # 1 - modul; 2 - misol
+
+# def takrorla(matn, n):
+#     return matn * n
+# natija = takrorla(input("Matnni kiriting: "), int(input("Nechta takrorlash kerak: ")))
+# print(natija)
+
+# 1 - modul: 3 - misol -------------------------------------------------------------
+
+# def juftmi(raqam):
+#     if raqam % 2 == 0:
+#         return True
+#     else:
+#         return False
+# natija = juftmi(int(input("sonni kiriting:")))
+# print(natija)
+
+# 1 - modul; 4 - misol -------------------------------------------------------------
+
+def yigindi(son1, son2):
+    return f"Yig'indi: {son1 + son2}"
+son1 = input("1-Sonni kiriting: ")
+son2 = input("2-Sonni kiriting: ")
+print(yigindi(son1, son2,))
